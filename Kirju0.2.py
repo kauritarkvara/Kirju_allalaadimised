de76f82195b2839f05b76d89354e078ce783dd30
kauritarkvara <162898993+kauritarkvara@users.noreply.github.com>
2024-04-20 14:16:11 +0300

    Kirju  0.2

--- Kirju0.2.py
+++ Kirju0.2.py
@@ -5,7 +5,7 @@ import time
 def muutus():
     tekst.pack(fill="both", expand=True)
 def teave():
-    messagebox.showinfo("Teave", "Kirju - Pythoniga tehtud kirjutamiseprogramm, mis on kerge! Versioon 1.0. Veebileht kauritarkvara.github.io/Kirjutamiseprogramm")
+    messagebox.showinfo("Teave", "Kirju - Pythoniga tehtud kirjutamiseprogramm, mis on kerge! Versioon 0.2. Veebileht kauritarkvara.github.io/Kirjutamiseprogramm")
 def sule():
     def välju():
        exit()
